5c86d1e226e5597c393341f0f791c7193d8aa7e3
BBird40 <bryanjebyrd@gmail.com>
2023-08-08 22:48:43 -0400

    tweak to controller

--- src/app/controllers/ExampleController.ts
+++ src/app/controllers/ExampleController.ts
@@ -11,35 +11,35 @@ import { ExampleRequest } from '../models/ExampleRequest';
 import { ExampleResponse } from '../models/ExampleResponse';
   
 @controller('/example')
-export class ExampleController{
+export class ExampleController {
 
 @httpPost('/create')
 public async createExample(
     @response() res: Response,
     @requestBody() newExample: ExampleRequest,
 ): Promise<ExampleResponse | undefined> {
-    try {
-    return new ExampleResponse();
-    } catch (error: unknown) {
-    res.status(500);
-    if (error instanceof Error) {
-        res.send(error.message);
+        try {
+            return new ExampleResponse();
+        } catch (error: unknown) {
+            res.status(500);
+            if (error instanceof Error) {
+                res.send(error.message);
+            }
+        }
     }
-    }
-}
 
 @httpGet('/:id')
 public async getExample(
     @response() res: Response,
     @requestParam('id') idParam: string,
 ): Promise<ExampleResponse | undefined> {
-    try {
-    return new ExampleResponse();
-    } catch (error: unknown) {
-    res.status(500);
-    if (error instanceof Error) {
-        res.send(error.message);
+        try {
+            return new ExampleResponse();
+        } catch (error: unknown) {
+            res.status(500);
+            if (error instanceof Error) {
+                res.send(error.message);
+            }
+        }
     }
-    }
-}
 }
